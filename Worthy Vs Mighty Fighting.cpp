f5f8349196d3cd4a576408ff817cd79a18f61de7
DwarfmanDDH <levi4723@gmail.com>
2016-06-17 11:36:48 -0600

    The finished(for now) fighting of base of the game.
    instead of using gotos it is now while loops and ifs

---
+++ Worthy Vs Mighty Fighting.cpp
@@ -0,0 +1,326 @@
+#include "stdafx.h"
+#include <character>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include 
+using namespace std;
+int Fighting()
+{
+    game = true
+    while game = true
+    {
+        StartofGame:
+        //Varibles
+        string playagian, Teams;
+        bool teamb, charchooseb, ifcharCb, ifcharFb;
+        //Combat Basics
+        //Choose Team
+        while charchooseb = true
+        {
+            cout << "What team do you want to be" << endl;
+            cout << "Worthy or Mighty" << endl;
+            cin >> Teams;
+            if (Teams == "Worthy"){
+                charchooseb, teamb = false
+                ifcharCb, ifcharFb = true
+                cout << "Who Would You like To Use" << endl;
+                cout << charlistW() << endl;
+                string champion = cin ">";
+                while ifcharCb = true
+                {
+                    //Recall if Playing Worthy
+                    if (champion = "Juggernaut"){
+                        champion = Juggernaut;
+                        ifcharCb = false;                        
+                    }
+                    else if (champion = "Hulk"){
+                        champion = Hulk;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Thing"){
+                        champion = Thing;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Attuma"){
+                        champion = Attuma;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Grey Gargoyle"){
+                        champion = GGargoyle;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Titania"){
+                        champion = Titania;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Absorbingman"){
+                        champion = Absorbingman;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Sin"){
+                        champion = Sin;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Serpent"){
+                        champion = Serpent;
+                        ifcharCb = false;
+                    }
+                    else {
+                        ifcharCb = true;
+                        cout << "Im^A1lb 1mqvt7" << endl;
+                    }
+                }
+                cout << "Who Would You like to fight" << endl;
+                cout << charlistM() << endl;
+                string fighter = cin >> ">";
+                while ifcharFb = true
+                {
+                    //Recall if fighting Mighty
+                    if (champion = "Wolverine"){
+                        champion = Wolverine;
+                        ifcharFb = false;
+
+                    }
+                    else if (champion = "Ms. Marvel"){
+                        champion = MsMarvel;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Iron Man"){
+                        champion = IronMan;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Iron Fist"){
+                        champion = IronFist;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Hawkeye"){
+                        champion = Hawkeye;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Red She Hulk"){
+                        champion = RedSheHulk;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Black Widow"){
+                        champion = BlackWidow;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Dr.Strange"){
+                        champion = DrStange;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Spider-Man"){
+                        champion = SpiderMan;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Captian America"){
+                        champion = GoodOleCap;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "The Mighty Thor"){
+                        champion = GodofThunder;
+                        ifcharFb = false;
+                    }
+                    else{ 
+                        ifcharFb = true;
+                        cout <<  "Sorry Im^A1lb 1mqvt7" << endl;
+                    }
+                }
+                
+            }
+            else if (Teams == "Mighty"){
+                teamb, ifcharCb, ifcharFb = true
+                charchooseb = false
+                cout << "Who Would You like To Use" << endl;
+                cout << charlistM() << endl;
+                string champion = cin ">";
+                while ifcharCb = true
+                {
+                    //Recall if Playing Mighty
+                    if (champion = "Wolverine"){
+                        champion = Wolverine;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Ms. Marvel"){
+                        champion = MsMarvel;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Iron Man"){
+                        champion = IronMan;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Iron Fist"){
+                        champion = IronFist;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Hawkeye"){
+                        champion = Hawkeye;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Red She Hulk"){
+                        champion = RedSheHulk;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Black Widow"){
+                        champion = BlackWidow;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Dr.Strange"){
+                        champion = DrStange;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Spider-Man"){
+                        champion = SpiderMan;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "Captian America"){
+                        champion = GoodOleCap;
+                        ifcharCb = false;
+                    }
+                    else if (champion = "The Mighty Thor"){
+                        champion = GodofThunder;
+                        ifcharCb = false;
+                    }
+                    else{ 
+                        ifcharCb = true;
+                        cout <<  "Sorry Im^A1lb 1mqvt7" << endl;
+                    }
+                }
+                cout << "Who Would You like to fight" << endl;
+                cout << charlistW() << endl;
+                string fighter = cin >> ">";
+                while ifcharFb = true;
+                {
+                    //Recall if fighting Worthy
+                    if (champion = "Juggernaut"){
+                        champion = Juggernaut;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Hulk"){
+                        champion = Hulk;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Thing"){
+                        champion = Thing;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Attuma"){
+                        champion = Attuma;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Grey Gargoyle"){
+                        champion = GGargoyle;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Titania"){
+                        champion = Titania;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Absorbingman"){
+                        champion = Absorbingman;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Sin"){
+                        champion = Sin;
+                        ifcharFb = false;
+                    }
+                    else if (champion = "Serpent"){
+                        champion = Serpent;
+                        ifcharFb = false;
+                    }
+                    else {
+                        ifcharb = true
+                        cout << "Im^A1lb 1mqvt7" << endl;
+                    }
+                }
+            }
+            else{
+                charchooseb = true;
+                cout << "Im^A1lb 1mqvt7" << endl;
+        }
+        }
+        //Champion Bio and attacks
+        cout << "Champion Name: " << champion.name << endl;
+        cout << "Champion Weapon: " << champion.weapon << endl;
+        cout << "champion's Real Name: " << champion.realname << endl;
+        //Rounds loop
+        int Rounds = 1
+        while healthC > 0 or healthF > 0
+        {
+            roundpt1:
+            cout << "what attack do you want to use" << endl;
+            cout << attack1S << "		" << attack2S << endl;
+            cout << attack3S << "		" << attack4S << endl;
+            cout << "Use Numbers to choose attacks";
+            int roundchoicesC = cin >> ">";
+            //Check if the attack can be done 
+            if (roundchoicesC == 1){
+                attackC = attack1C
+            }
+            else if (roundchoicesC == 2){
+                attackC = attack2C
+            }
+            else if (roundchoicesC == 3){
+                attackC = attack3C
+            }
+            else if (roundchoicesC == 4){
+                attackC = attack4C
+            }
+            else {
+                cout << "Im^A1lb Choice of Attack";
+                goto roundpt1;
+            }
+
+            int randomattack = rand() % 4 + 1
+            int roundchoicesF = randomattack
+
+            if (roundchoicesF = 1){
+                attackF = attack1F
+            }
+            else if (roundchoicesF = 2){
+                attackF = attack2F
+            }
+            else if (roundchoicesF = 3){
+                attackF = attack3F
+            }
+            else if (roundchoicesF = 4){
+                attackF = attack4F
+            }
+    //Health of the Fighter 
+            dmgC = attackF
+            healthF = healthF - dmgC
+    //Health of the Campion
+            int dmgF = attackC
+            int healthC = healthC - dmgF
+            int Rounds = Rounds + 1
+        }
+        string gameend;
+        if (healthC = 0 and healthF = 0){
+            gameend = "Both of you Died! Do you want to try again?";
+        } 
+        else if (healthC = 0 and healthF > 0){
+            gameend = "You Died! Do you want to try again?"
+        }
+        else if (healthC < 0 and healthF = 0){
+            gameend = fighter, "Died and you win! Do you want to play agian?"
+        }
+        while EndGame = true
+        {
+            cout << gameend << endl;
+        cout << "Yes or No" << endl;
+        cin >> playagain;
+        if (playagain = "Yes"){
+            game = true;
+            EndGame = false;
+        }
+        else if (playagain = "No"){
+            game, EndGame = false;
+        }
+        else {
+            gameend = "Im^A1lb 1mqvt7";
+            EndGame = true;
+        }
+    }
+    }
+}
